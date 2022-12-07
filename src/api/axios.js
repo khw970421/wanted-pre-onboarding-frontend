4cb4e819c79d51e79d8f14b9607dbf056cbc5dc9
khw970421 <khw970421@kakao.com>
2022-12-07 17:41:23 +0900

    ✨ feat : token 유무에 따른 API 호출

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,17 +3,29 @@ import axios from "axios";
 const API = "https://pre-onboarding-selection-task.shop";
 
 // Todos : 중복된 아이디 만들경우 에러 핸들링
-const request = async (subUrl, body) => {
+const request = async (subUrl, body, token = "") => {
   try {
-    const res = await axios.post(`${API}${subUrl}`, body, {
-      Authorization: `Bearer ${process.env.SERVICE_KEY}`,
-    });
-    // 호출 중 에러가 발생했을 때의 처리
-    if (res.status === 201 || res.status === 200) {
-      return res.data;
+    if (token) {
+      const res = await axios.post(`${API}${subUrl}`, body, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // 호출 중 에러가 발생했을 때의 처리
+      if (res.status === 201 || res.status === 200) {
+        return res.data;
+      }
+      // 오류가 발생한 경우를 체크
+      throw new Error("API 호출 오류");
+    } else {
+      const res = await axios.post(`${API}${subUrl}`, body);
+      // 호출 중 에러가 발생했을 때의 처리
+      if (res.status === 201 || res.status === 200) {
+        return res.data;
+      }
+      // 오류가 발생한 경우를 체크
+      throw new Error("API 호출 오류");
     }
-    // 오류가 발생한 경우를 체크
-    throw new Error("API 호출 오류");
   } catch (e) {
     // 오류가 발생했음을 사용자에게 인지
     alert(e.message);
